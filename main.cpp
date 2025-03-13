605b594c632379cfd4fd6a136b921e1b72e2de37
OkayJul <143652829+OkayJul@users.noreply.github.com>
2025-03-13 08:10:04 -0500

    main.cpp
    Updated to add Queue functionality and broad compatibility with linked-lists in progress

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 
+
 // node structure for both stack and queue
 struct Node {
   // data value
@@ -35,12 +36,75 @@ public:
   Node* operator->() { return current; }
 };
 
+class Queue {
+  private:
+      Node* front;
+      Node* end;
+      //linkedlist list; // holds linked list
+  public:
+      // Constructor
+      Queue() : front(nullptr), end(nullptr) {}
+      //Constructor (initialized with an existing linked list) WIP
+      /*Queue(linkedlist list) {
+        this->front = list->head;
+        this->end = list->tail; 
+      }*/
+      ~Queue() { 
+        while (front) {
+          Node* temp = front;
+          front = front->next;
+          delete temp;
+        }
+      }
+      // Enqueue function to add elements to the queue
+      void enqueue(int value) {
+        Node* temp = new Node(value);
+        if (!end) {
+            front = end = temp;
+        } 
+        else{
+          end->next = temp;
+          end = temp;
+        }
+      }
+  
+      // Dequeue function to remove elements from the queue
+      void dequeue() {
+        if (!front){
+          std::cout << "Queue is empty" << std::endl;
+          return;
+        }
 
+        Node* temp = front;
+        int value = front->data;
+        front = front->next;
+        if (!front) {
+          end = nullptr;
+        }
+        delete temp;
+        std::cout << "Node with Value: " << value << " Successfully Removed!" << std::endl;
+      }
+
+      //view queue function to print all elements in queue
+      void viewqueue() {
+        if (!front){
+          std::cout << "No Data to View" << std::endl;
+          return;
+        }
+
+        ListIterator it(front);
+        while (it != ListIterator(nullptr)){
+          std::cout << *it << "  ";
+          ++it;
+        }
+        std::cout << std::endl;
+      }
+};
 
 
 void initialMenu();
 void stackMenu();
-void queueMenu();
+void queueMenu(Queue& Que);
 int main(){
   std::cout << "hello World";
   std::cout << "test";
@@ -52,6 +116,7 @@ int main(){
 //Use a boolean value to see if the menu should display a welcome message.
 //only called once for the entire program duration.
 void initialMenu () {
+  Queue Que;
   std::cout <<"\nWelcome" << std::endl;
   bool choiceMade = false;
   do {
@@ -65,7 +130,7 @@ void initialMenu () {
         break;
       }
       case 2: {
-        queueMenu();
+        queueMenu(Que);
         break;
       }
       case 3: {
@@ -113,23 +178,26 @@ void stackMenu() {
   }while (true);
 
 }
-void queueMenu() {
+void queueMenu(Queue& Que) {
   do {
     int choice = 0;
+    int value;
     std::string menu = "Please enter the desired queue action\n1.Enqueue\n2.Dequeue\n3.View entire Queue\n4.Return to main menu.\n";
     std::cout << menu;
     std::cin >> choice;
     switch (choice) {
       case 1: {
-        //TODO call enqueue logic
+        std::cout << "Enter Interger Value to Queue: ";
+        std::cin >> value;
+        Que.enqueue(value);
         break;
       }
       case 2: {
-        //TODO call dequeue logic
+        Que.dequeue();
         break;
       }
       case 3: {
-        //TODO call view queue logic
+        Que.viewqueue();
         break;
       }
       case 4: {
